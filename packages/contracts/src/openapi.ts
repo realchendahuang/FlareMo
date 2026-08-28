ace36c9e4bbb820cf20b432987aa4fbfbaf892b0
Kim <realchendahuang@gmail.com>
2026-08-29 00:38:22 +0800

    chore(release): prepare v0.14.0

--- packages/contracts/src/openapi.ts
+++ packages/contracts/src/openapi.ts
@@ -21,7 +21,7 @@ import {
   updateMemoSchema,
 } from "./memos";
 
-export const FLAREMO_API_VERSION = "0.13.0";
+export const FLAREMO_API_VERSION = "0.14.0";
 
 type JsonSchema = Record<string, unknown>;
 
